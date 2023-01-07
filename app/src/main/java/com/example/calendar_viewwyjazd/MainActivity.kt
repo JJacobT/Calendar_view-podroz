713745f5115a177f745d053b2867a6fb70bb0532
JJacobT <tyszko0526@wp.pl>
2023-01-07 15:26:36 +0100

    dodane dzialanie przyciskow

--- app/src/main/java/com/example/calendar_viewwyjazd/MainActivity.kt
+++ app/src/main/java/com/example/calendar_viewwyjazd/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             }
             return formDate
         }
-        //zmienne
+        //Zmienne
         val wyjazd = findViewById<Button>(R.id.wyjazd_button)
         val powrot = findViewById<Button>(R.id.przyjazd_button)
         val kalendarz = findViewById<CalendarView>(R.id.calendarView)
@@ -36,9 +36,10 @@ class MainActivity : AppCompatActivity() {
         val start = mutableListOf(0,0,0)
         val koniec = mutableListOf(0,0,0)
 
+
         //Blokada
         kalendarz.minDate = Date().time
-        kalendarz.maxDate = Date().time + 88888888888
+        kalendarz.maxDate = Date().time + 109572660000000
         //Lista
         kalendarz.setOnDateChangeListener { _, d, m, y ->
             data[0] = y
@@ -53,6 +54,22 @@ class MainActivity : AppCompatActivity() {
             val ilosc_dni = wyjazd.toChar() - przyjazd.toChar()
             czas_trwania.text = "${ilosc_dni.absoluteValue + 1}"
         }
+        //Przyciski
+        obliczanie.setOnClickListener {
+            rezerwacja(start, koniec, czas)
+        }
+        powrot.setOnClickListener {
+            for (i in 0 until 3)
+                start[i] = data[i]
+            powrot_txt.text =  "${start[0]}-${start[1]}-${start[2]}"
+        }
+
+        wyjazd.setOnClickListener {
+            for (i in 0 until 3)
+                koniec[i] = data[i]
+            wyjazd_txt.text = "${koniec[0]}-${koniec[1]}-${koniec[2]}"
+        }
+
 
 
     }
