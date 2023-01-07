a8c3e18ba6e0922d12b429e32f2a7b17e542694f
JJacobT <tyszko0526@wp.pl>
2023-01-07 15:03:11 +0100

    dodana konwersja czasu i podpiete zmienne

--- app/src/main/java/com/example/calendar_viewwyjazd/MainActivity.kt
+++ app/src/main/java/com/example/calendar_viewwyjazd/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.calendar_viewwyjazd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextClock
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Własciwy format daty
+        fun konwersja(czasowa: Long): List<Int>{
+            val date = Date(czasowa)
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+            val formDate = dateFormat.format(date).split("/").map {
+                it.toInt()
+            }
+            return formDate
+        }
+        //zmienne
+        val wyjazd = findViewById<Button>(R.id.wyjazd_button)
+        val powrot = findViewById<Button>(R.id.przyjazd_button)
+        val kalendarz = findViewById<CalendarView>(R.id.calendarView)
+        val obliczanie = findViewById<Button>(R.id.zaplanuj_button)
+        val data = arrayListOf(konwersja(kalendarz.date)[0] ,konwersja(kalendarz.date)[1] ,konwersja(kalendarz.date)[2])
+        val czas = findViewById<TextView>(R.id.wynik_txtview)
+        val wyjazd_txt = findViewById<TextView>(R.id.wyj_txtview)
+        val powrot_txt = findViewById<TextView>(R.id.przyj_txtview)
+        val start = mutableListOf(0,0,0)
+        val koniec = mutableListOf(0,0,0)
+
     }
 }
